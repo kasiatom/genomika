00578d4cebeee73f1c4ad59bb204911a1b7eb32f
Katarzyna Tomala <katarzyna.tomala@uj.edu.pl>
2020-04-14 20:02:46 +0200

    ćwiczenia4 initial

---
+++ cwiczenia4/Instrukcje4.md
@@ -0,0 +1,72 @@
+## SARS-CoV-2
+
+Celem ćwiczenia będzie analiza danych dotyczących epidemii COVID-19
+SARS-CoV-2.   
+
+### Zadanie1 
+#### Drzewo filogenetyczne betakoronawirusów  
+Proszę obejrzeć drzewo filogenetyczne betakoronawirusów [link](https://nextstrain.org/groups/blab/sars-like-cov)  
+i odpowiedzieć na następujące pytania:
+1. Jak nazywa się wirus najbliżej spokrewniony z wirusem SARS-CoV-2, 
+jaki gatunek jest jego gospodarzem (wystarczy nazwa potoczna)? 
+Czy jest to już dowód na to, że do transmisji wirusa SARS-CoV-2 na człowieka doszło właśnie z tego gatunku?
+Proszę uzasadnić. 
+2. Czy transmisja między gatunkami jest czymś wyjątkowym? Jaki gatunek był najbardziej
+ prawdopodobnie pierwotnym gospodarzem wirusa SARS-CoV? 
+ 
+Jakie jest pochodzenie innych wirusów atakujacych człowieka (HIV, Ebola)?
+
+### Zadanie2 
+#### Drzewo filogenetyczne wirusa SARS-CoV-2
+Proszę wejść na stronę [nextstrain](https://nextstrain.org/ncov/global) dotyczącą wirusa SARS-CoV-2. 
+Proszę obejrzeć animację i rozejrzeć się na stronie, tak aby znaleźć odpowiedzi na poniższe pytania. Pomocne mogą 
+okazać sie przygotowane przez tworców strony tutoriale 
+[*How to interpret the phylogenetic trees*](https://nextstrain.org/help/general/how-to-read-a-tree) 
+oraz [*Exploring interactive phylogenies with Auspice*](https://neherlab.org/201901_krisp_auspice.html).
+1. Czy wszystkie wirusy SARS-CoV-2 zostały wyizolowane z człowieka? 
+2. Jak wirus dotarł do Ameryki Północnej - bezpośrednio z Chin, czy z Europy?
+3. Ile kladów wirusa jest wyodrębnionych na stronie, jaka mutacja charakteryzuje najliczniejszy
+ klad, jak się ten klad nazywa?
+4. Powyższa mutacja dotyczy białka S koronawirusa. 
+Proszę przeczytać artykuł [*Structural basis for the recognition of SARS-CoV-2 by full-length human ACE2*](https://science.sciencemag.org/content/367/6485/1444). 
+Jaką rolę pełni to białko podczas infekcji? Proszę wypisać, które pozycje tego białka wydają się być 
+ważne dla jego funkcji (Fig4, tekst)
+5. Proszę teraz sprawdzić, czy opisano mutacje tych pozycji u wirusa SARS-CoV-2. 
+Jeśli tak, to proszę je wypisać. Czy ewentualna obecność takich mutacji wskazuje na omyłkę autorów powyższej publikacji?
+Prosze uzasadnić.
+6. Czym różnią się dwie zastosowane na stronie nextstrain miary zmienności danej pozycji wirusa (*ENTROPY* vs *EVENTS*)?
+O czym mogą świadczyć wysokie warości tych miar obserwowane dla niektórych pozycji w genomie wirusa? 
+
+### Zadanie3
+#### Czy wirus z czasem staje się mniej zjadliwy? Czy częściej atakuje mężczyzn? Czy są mutacje ułatwiające infekcję osób młodych?
+
+Na stronie `http://data.nextstrain.org/ncov_global.json` znajdują sie dane, które posłużyły do otrzymania
+ analizowanego w poprzednim ćwiczeniu drzewa filogenetycznego wirusa w formacie json. Są one podzielone na trzy
+  główne części (meta, tree, version). Proszę pobrać ze strony plik z danymi, a nastepnie "wyciąć" z niego 
+  cześć dotyczącą drzewa:
+  ```bash
+wget http://data.nextstrain.org/ncov_global.json
+cat ncov_global.json | jq .tree > ncov-tree.json
+  ``` 
+Proszę także pobrać ze strony ćwiczeń skrypt ułatwiający parsowanie pliku `ncov-tree.json`
+ (program kopiuje rozwiązanie podane tutaj
+  [link](https://towardsdatascience.com/flattening-json-objects-in-python-f5343c794b10): 
+```bash
+wget 'https://github.com/genomika-2020/genomika/raw/master/cwiczenia4/ncov-tree-parser.py' 
+
+## pomoc 
+python3 ncov-tree-parser.py --help
+
+## przykładowe użycie: 
+## wynikowy plik to tabela z id szczepu, wiekiem pacjenta i listą mutacji (aa) w kolejnych kolumnach
+python3 ncov-tree-parser.py --input ncov-tree.json --output ncov-table.tab --features "age,mutations"
+```
+Proszę spróbowac przeanalizować pobrane dane, tak aby odpowiedzieć na **jedno** z powyższych pytań. 
+Proszę opisać przeprowadzone analizy, podać wnioski oraz założenia, jakie musieli państwo poczynić,
+przy analizie danych i interpretacji wyników.   
+Uwaga: dane można przeanalizować dowolnie, skrypt ncov-tree-parser.py ma państwu ułatwić zadanie i nie trzeba go 
+używać.
+
+***
+***
+ [Powrót do strony zajęć](https://github.com/genomika-2020/genomika/blob/master/README.md) 
---
+++ cwiczenia4/ncov-tree-parser.py
@@ -0,0 +1,143 @@
+import json
+import argparse
+
+names = {
+        "country":         "node_attrs_country_value",
+        "divergence":      "node_attrs_div",
+        "location":        "node_attrs_location_value",
+        "date":            "node_attrs_num_date_value",
+        "sex":             "node_attrs_sex_value",
+        "age":             "node_attrs_age_value",
+        "mutations":       "branch_attrs_labels_aa",
+        "clade":           "node_attrs_clade_membership_value"
+        
+        }
+
+
+def get_keys_by_args(arg_name):
+    return names[arg_name]   
+
+
+def read_feature_arg(text):
+    return text.split(",")
+
+
+def get_full_key_name(prefix, feature_list):
+    return [prefix + x for x in feature_list]
+
+
+def prepare_all_keys(aa_key):
+    aa_keys_final_list = []
+    aa_key_list = aa_key.replace("_branch_attrs_labels_aa", "").split("_")
+    for i in range(2,len(aa_key_list),2):
+        new_key = "_".join(aa_key_list[0:i]) +"_branch_attrs_labels_aa"
+        #print(new_key)
+        aa_keys_final_list.append(new_key)
+    return aa_keys_final_list
+
+
+def safe_get_value(dictionary, one_key):
+    if "branch_attrs_labels_aa" in one_key:
+        
+        v1 = ""
+        for i in prepare_all_keys(one_key):
+            try:
+                v1 += "; " + dictionary[i]
+            except KeyError:
+                v1 += ""
+        v = order_mutation_list(v1.strip(";").strip()) 
+    else:
+        try:
+            v = str(dictionary[one_key])
+        except KeyError:
+            v = ""
+    return v
+
+
+def order_mutation_list(mutations):
+    if mutations != "":
+        messy_list = [i.split(": ") for i in mutations.split("; ")] 
+        print(messy_list)
+        genes = set(sorted(([i[0] for i in messy_list])))
+        nice_list = []
+        for gene in genes:
+            gene_mutations = ", ".join(["".join(i[1]) for i in messy_list if i[0] == gene ]).split(", ")
+            print(gene_mutations) 
+            sorted_mutations = sorted(gene_mutations, key = lambda x: int(x[1:-1]))
+            final_mutations = gene + ": " + ", ".join(sorted_mutations)
+            nice_list.append(final_mutations)
+        return "; ".join(nice_list)
+    else:
+        return ""
+
+
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+
+def find_good_values(big_dict, features_list):
+    out = []
+    keys_list = []
+    header = "ID\t"
+    for feature in features_list:
+        try:
+            keys_list.append(get_keys_by_args(feature))
+            header+=feature+"\t"
+        except KeyError:
+            print("Invalid feature value: "+feature+". Skipped")
+            continue
+    selected_keys = [item for item in big_dict.keys() if "gisaid_epi_isl_value" in item]
+    selected_names = [name.replace("node_attrs_gisaid_epi_isl_value","") for name in selected_keys ]
+    for i in selected_names:
+        ID = [big_dict[i + "node_attrs_gisaid_epi_isl_value"]]
+        new_keys = get_full_key_name(i, keys_list) ##tutaj
+        items = ID + [safe_get_value(big_dict, i) for i in new_keys]
+        out.append(items)
+    return [out, header]
+
+
+if __name__=='__main__':
+   
+    parser = argparse.ArgumentParser(description='This script flatten the tree part of the ncov json and returns needed node and branch attributes in tab format ')
+    parser.add_argument('-i', '--input', type=str, help="Path to input ncov tree json; created with 'cat ncov_global.json | jq .tree' command")
+    parser.add_argument('-f', '--features', type=str, help="Coma deliminated list of wanted attributes, \
+                                                            for example 'age,mutations', 'clade,sex,date,divergence'. \
+                                                            ................ Possible values: ....................\
+                                                            country, location, date (output given as \
+                                                            year.year_fraction),  \
+                                                             sex, mutations (list of all protein level mutations), \
+                                                            clade, age, divergence (virus sequence divergence, \
+                                                            comparing to the initial Wuhan strais)")
+                                           
+    parser.add_argument('-o', '--output', type=str, help="Path to output tab file")
+    args = parser.parse_args()
+
+    t=open(args.input)
+    ncov = json.load(t)
+    t.close()
+    wanted_features = read_feature_arg(args.features)
+    flat_json = (flatten_json(ncov))
+    data, header = find_good_values(flat_json, wanted_features)
+    
+    with open(args.output, 'w') as f:
+        f.write(header+"\n")
+        for item in data:
+            new_item = "\t".join(item)+"\n"
+            f.write(new_item) 
+
+
